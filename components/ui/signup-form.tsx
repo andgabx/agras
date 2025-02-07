19f71186438c14bd0e3e1fe99b787a7f768bb402
vinivent <vini.ventura999@gmail.com>
2025-02-07 20:29:23 -0300

    css

--- components/ui/signup-form.tsx
+++ components/ui/signup-form.tsx
@@ -52,32 +52,31 @@ export function SignupForm({
         />
       </div>
       <div className="flex flex-col space-y-4 gap-2 text-center">
-        <div className="flex justify-start items-start flex-col mb-8">
+        <div className="flex justify-start items-start flex-col mb-6">
           <h1 className="text-[50px] font-bold mb-1">Cadastre-se</h1>
-          <p className="text-balance text-xl font-bold">
+          <p className="text-balance text-[18px] font-medium">
             Seja bem vindo(a) ao AGRAS!
           </p>
         </div>
       </div>
       <div className="grid gap-6">
         <div className="grid gap-2">
-          <Label htmlFor="email">Email</Label>
           <Input
             id="email"
             type="email"
-            placeholder="seuemail@exemplo.com"
+            placeholder="Email"
+            className=""
             required
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="grid gap-2">
-          <div className="flex items-center">
-            <Label htmlFor="password">Senha</Label>
-          </div>
           <Input
             id="password"
             type="password"
+            placeholder="Senha"
+            className=""
             required
             value={password}
             onChange={(e) => setPassword(e.target.value)}
@@ -85,13 +84,13 @@ export function SignupForm({
         </div>
         <Button
           type="submit"
-          className="w-full bg-green-600"
+          className="w-full bg-[#8ABF17]"
           disabled={loading}
         >
-          Cadastre-se
+          Cadastrar
         </Button>
         <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-          <span className="relative z-10 bg-background px-2 text-green-600">
+          <span className="relative z-10 bg-background px-2 text-[#8ABF17]">
             Ou
           </span>
         </div>
@@ -102,8 +101,8 @@ export function SignupForm({
       </div>
       <div className="text-center text-sm">
         Já tem uma conta?{" "}
-        <Link href="/login" className="underline underline-offset-4">
-          Faça login
+        <Link href="/" className="underline underline-offset-4 text-[#8ABF17]">
+          Entre aqui
         </Link>
       </div>
     </form>
