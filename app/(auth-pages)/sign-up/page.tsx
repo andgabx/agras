2227c715a87e1808eaca83127790e90519f460db
Anderson Gabriel <gabrielvalencasa98@gmail.com>
2025-02-10 13:20:03 -0300

    Update sign-up page design and add terms checkbox

--- app/(auth-pages)/sign-up/page.tsx
+++ app/(auth-pages)/sign-up/page.tsx
@@ -5,6 +5,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import Image from "next/image";
+import { Checkbox } from "@/components/ui/checkbox";
 
 export default async function Signup(props: {
   searchParams: Promise<Message>;
@@ -23,24 +24,17 @@ export default async function Signup(props: {
       <div className="flex items-center justify-center w-full lg:w-1/2 p-8">
         <form className="w-full max-w-md">
           <div className="flex flex-col gap-2">
-            <h1 className="text-2xl font-medium">Sign up</h1>
-            <p className="text-sm text text-foreground">
-              Already have an account?{" "}
-              <Link
-                className="text-primary font-medium underline"
-                href="/sign-in"
-              >
-                Sign in
-              </Link>
-            </p>
+            <Image className="mx-auto" src="/agrasloginlogo.png" alt="Logo" width={300} height={100} />
+            <h1 className="text-2xl font-medium">Cadastre-se</h1>
+            
             <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-              <Label htmlFor="name">Nome</Label>
-              <Input name="name" placeholder="Your name" required />
-              <Label htmlFor="phone">Telefone</Label>
-              <Input name="phone" placeholder="Your phone" required />
-              <Label htmlFor="email">Email</Label>
-              <Input name="email" placeholder="you@example.com" required />
-              <Label htmlFor="password">Senha</Label>
+              <Label className="font-bold" htmlFor="name">Nome</Label>
+              <Input name="name" placeholder="Seu nome" required />
+              <Label className="font-bold" htmlFor="phone">Telefone</Label>
+              <Input name="phone" placeholder="Seu telefone" required />
+              <Label className="font-bold" htmlFor="email">Email</Label>
+              <Input name="email" placeholder="seu@email.com" required />
+              <Label className="font-bold" htmlFor="password">Senha</Label>
               <Input
                 type="password"
                 name="password"
@@ -48,6 +42,10 @@ export default async function Signup(props: {
                 minLength={6}
                 required
               />
+              <div className="flex items-center gap-2">
+                <Checkbox id="terms" required />
+                <Label className="font-bold text-sm">Eu concordo com as políticas de privacidade e termos de uso</Label>
+              </div>
               <SubmitButton
                 className="bg-[#8ABF17] text-white"
                 formAction={signUpAction}
@@ -56,6 +54,15 @@ export default async function Signup(props: {
                 Criar conta
               </SubmitButton>
               <FormMessage message={searchParams} />
+              <p className="text-sm text text-foreground">
+              Já tem uma conta?{" "}
+              <Link
+                className="text-[#8ABF17] font-medium underline"
+                href="/sign-in"
+              >
+                Entre aqui
+              </Link>
+            </p>
             </div>
           </div>
         </form>
