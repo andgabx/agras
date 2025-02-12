af87f0ed615f4d9c577c00388d3322735d385765
Anderson Gabriel <gabrielvalencasa98@gmail.com>
2025-02-11 22:29:54 -0300

    feat: Enhance authentication pages with logo and improved UI details

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -13,14 +13,23 @@ export default async function Login(props: { searchParams: Promise<Message> }) {
     <div className="flex h-screen">
       <div className="w-full lg:w-1/2 flex items-center justify-center px-8">
         <form className="flex-1 flex flex-col max-w-md">
+          <Image
+            className="mx-auto pb-16"
+            src="/agrasloginlogo.png"
+            alt="Logo"
+            width={300}
+            height={100}
+          />
+
           <h1 className="text-5xl font-bold">Login</h1>
-          <p className="text-xl py-4 font-medium text-foreground">Seja bem vindo(a) novamente!</p>
+          <p className="text-xl py-4 font-medium text-foreground">
+            Seja bem vindo(a) novamente!
+          </p>
           <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
             <Label htmlFor="email">Email</Label>
             <Input name="email" placeholder="exemplo@email.com" required />
             <div className="flex justify-between items-center">
               <Label htmlFor="password">Senha</Label>
-
             </div>
             <Input
               type="password"
@@ -31,12 +40,14 @@ export default async function Login(props: { searchParams: Promise<Message> }) {
             <div className="flex justify-between items-center">
               <div className="flex items-center gap-2 py-4">
                 <Checkbox id="remember" />
-                <Label className="text-md" htmlFor="remember">Lembrar-me</Label>
+                <Label className="text-md" htmlFor="remember">
+                  Lembrar-me
+                </Label>
               </div>
-            <Link
-              className="text-md text-[#8ABF17] underline py-4"
-              href="/forgot-password"
-            >
+              <Link
+                className="text-md text-[#8ABF17] underline py-4"
+                href="/forgot-password"
+              >
                 Esqueceu sua senha?
               </Link>
             </div>
@@ -56,7 +67,6 @@ export default async function Login(props: { searchParams: Promise<Message> }) {
             </Link>
           </p>
         </form>
-        
       </div>
       <div className="hidden lg:block relative w-1/2">
         <Image
--- app/(auth-pages)/sign-up/page.tsx
+++ app/(auth-pages)/sign-up/page.tsx
@@ -24,20 +24,34 @@ export default async function Signup(props: {
       <div className="flex items-center rounded-r-[150px] bg-white justify-center w-full lg:w-1/2 p-8">
         <form className="w-full max-w-md">
           <div className="flex flex-col gap-2">
-            <Image className="mx-auto pb-16" src="/agrasloginlogo.png" alt="Logo" width={300} height={100} />
+            <Image
+              className="mx-auto pb-16"
+              src="/agrasloginlogo.png"
+              alt="Logo"
+              width={300}
+              height={100}
+            />
             <h1 className="text-5xl font-bold">Cadastre-se</h1>
             <p className="text-xl font-medium text-foreground">
               Seba bem vindo(a) ao AGRAS!
             </p>
-            
+
             <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-              <Label className="font-bold" htmlFor="name">Nome</Label>
+              <Label className="font-bold" htmlFor="name">
+                Nome
+              </Label>
               <Input name="name" placeholder="Seu nome" required />
-              <Label className="font-bold" htmlFor="phone">Telefone</Label>
+              <Label className="font-bold" htmlFor="phone">
+                Telefone
+              </Label>
               <Input name="phone" placeholder="Seu telefone" required />
-              <Label className="font-bold" htmlFor="email">Email</Label>
+              <Label className="font-bold" htmlFor="email">
+                Email
+              </Label>
               <Input name="email" placeholder="seu@email.com" required />
-              <Label className="font-bold" htmlFor="password">Senha</Label>
+              <Label className="font-bold" htmlFor="password">
+                Senha
+              </Label>
               <Input
                 type="password"
                 name="password"
@@ -47,25 +61,40 @@ export default async function Signup(props: {
               />
               <div className="flex items-center gap-2">
                 <Checkbox id="terms" required />
-                <Label className="font-bold text-sm">Eu concordo com as políticas de privacidade e termos de uso</Label>
+                <Label className="font-bold py-4 text-sm">
+                  Eu concordo com as{" "}
+                  <Link
+                    className="text-[#8ABF17] font-bold text-md underline"
+                    href="#"
+                  >
+                    termos de uso
+                  </Link>{" "}
+                  e{" "}
+                  <Link
+                    className="text-[#8ABF17] font-bold text-md underline"
+                    href="#"
+                  >
+                    políticas de privacidade
+                  </Link>
+                </Label>
               </div>
               <SubmitButton
                 className="bg-[#8ABF17] text-white"
                 formAction={signUpAction}
                 pendingText="Criando conta..."
               >
-                Criar conta
+                Cadastrar
               </SubmitButton>
               <FormMessage message={searchParams} />
-              <p className="text-sm text text-foreground">
-              Já tem uma conta?{" "}
-              <Link
-                className="text-[#8ABF17] font-medium underline"
-                href="/sign-in"
-              >
-                Entre aqui
-              </Link>
-            </p>
+              <p className="text-sm font-medium text-center text-foreground">
+                Já tem uma conta?{" "}
+                <Link
+                  className="text-[#8ABF17] font-medium underline"
+                  href="/sign-in"
+                >
+                  Entre aqui
+                </Link>
+              </p>
             </div>
           </div>
         </form>
