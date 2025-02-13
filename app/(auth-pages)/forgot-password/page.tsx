be851812478bdb88af8b4064890e85230852c729
vinivent <vini.ventura999@gmail.com>
2025-02-13 18:14:36 -0300

    formulario de esqueci senha

--- app/(auth-pages)/forgot-password/page.tsx
+++ app/(auth-pages)/forgot-password/page.tsx
@@ -4,32 +4,71 @@ import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
+import Image from "next/image";
 
 export default async function ForgotPassword(props: {
   searchParams: Promise<Message>;
 }) {
   const searchParams = await props.searchParams;
   return (
-    <>
-      <form className="flex-1 flex flex-col w-full gap-2 text-foreground [&>input]:mb-6 min-w-64 max-w-64 mx-auto">
-        <div>
-          <h1 className="text-2xl font-medium">Reset Password</h1>
-          <p className="text-sm text-secondary-foreground">
-            Already have an account?{" "}
-            <Link className="text-primary underline" href="/sign-in">
-              Sign in
+    <div className="flex bg-[#FAFAFA] h-screen">
+      <div className="flex items-center rounded-none lg:rounded-r-[120px] bg-white shadow-lg justify-center w-full lg:w-1/2 p-8">
+        <form className="flex-1 flex flex-col max-w-md">
+          <Image
+            className="mx-auto pb-16"
+            src="/agrasloginlogo.png"
+            alt="Logo"
+            width={300}
+            height={100}
+          />
+
+          <h1 className="text-5xl font-bold">Resetar Senha</h1>
+          <p className="text-[17px] py-4 font-medium text-foreground">
+            Já tem uma conta?{" "}
+            <Link
+              className="text-[#8ABF17] hover:text-[#8ABF17]/90 underline"
+              href="/sign-in"
+            >
+              Faça login
             </Link>
           </p>
-        </div>
-        <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-          <Label htmlFor="email">Email</Label>
-          <Input name="email" placeholder="you@example.com" required />
-          <SubmitButton formAction={forgotPasswordAction}>
-            Reset Password
-          </SubmitButton>
-          <FormMessage message={searchParams} />
-        </div>
-      </form>
-    </>
+
+          <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
+            <Label htmlFor="email">Email</Label>
+            <Input name="email" placeholder="exemplo@email.com" required />
+            
+            <SubmitButton 
+              pendingText="Enviando..."
+              formAction={forgotPasswordAction}
+              className="mt-4"
+            >
+              Enviar Instruções
+            </SubmitButton>
+            
+            <FormMessage message={searchParams} />
+          </div>
+
+          <p className="text-center py-4 text-sm text-foreground">
+            Não recebeu o e-mail?{" "}
+            <Link
+              className="text-[#8ABF17] hover:text-[#8ABF17]/90 font-bold underline"
+              href="/forgot-password"
+            >
+              Reenviar
+            </Link>
+          </p>
+        </form>
+      </div>
+      <div className="hidden lg:block w-1/2 m-auto px-4">
+        <Image
+          src="/esqueci-novo2.png"
+          alt="Login"
+          width={650}
+          height={550}
+          className="mx-auto"
+          priority
+        />
+      </div>
+    </div>
   );
 }
