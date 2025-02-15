139e720d21e4cf7a116ed23fe3ddf3af630136c4
vinivent <vini.ventura999@gmail.com>
2025-02-14 23:48:43 -0300

    feat: update global styles and layout for improved font handling and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,9 @@
-"use client"; // Adicione esta linha no topo do arquivo
 
+import type { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
-import { useState, useEffect } from 'react';
-import Loading from "@/components/loading/Loading";
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
-
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -17,22 +11,16 @@ const poppins = Poppins({
   variable: "--font-poppins",
 });
 
+export const metadata: Metadata = {
+  title: "AGRAS",
+  description: "Sistema de gerenciamento de plantio",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [isLoading, setIsLoading] = useState(true); // Estado para controlar o loading
-
-  useEffect(() => {
-    // Simula um tempo de carregamento (substitua pela sua lógica real)
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 1500);
-
-    return () => clearTimeout(timer);
-  }, []);
-
 
   return (
     <html lang="en" className={poppins.variable} suppressHydrationWarning>
@@ -43,13 +31,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <main className="">
-            {isLoading ? (
-              <Loading />
-            ) : (
-              <div className="min-h-screen bg-background font-sans antialiased">{children}</div>
-            )}
-          </main>
+          {children}
           <Toaster />
         </ThemeProvider>
       </body>
