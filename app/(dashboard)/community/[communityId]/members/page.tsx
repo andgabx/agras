f4c774dd3677b287bd27363fe563de2658127c5c
vinivent <vini.ventura999@gmail.com>
2025-02-24 09:15:03 -0300

    refactor: enhance layout of members page with max width and centering

--- app/(dashboard)/community/[communityId]/members/page.tsx
+++ app/(dashboard)/community/[communityId]/members/page.tsx
@@ -6,7 +6,7 @@ export default async function Participantes({
   params: { communityId: string };
 }) {
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto">
       <MemberList communityId={params.communityId} />
     </div>
   );
